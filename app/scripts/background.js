76c78065b351cc3fee0c34e2003614cbf710b2f8
Manuel Möhlmann <mail@manmoe.com>
2013-11-07 10:53:14 +0100

    added comments

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,13 +4,22 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-// get authenticated
+/**
+ * Method for creating basic auth header.
+ *
+ * @param user The user who wants to authenticate
+ * @param password The password of the user who wants to authenticate
+ * @returns {string} The resulting header for basic auth to get the given user authenticated
+ */
 function make_base_auth(user, password) {
     var tok = user + ':' + password;
     var hash = btoa(tok);
     return "Basic " + hash;
 }
 
+/**
+ * Get all feeds from Github for the user and select the public feed.
+ */
 function getFeedUrl() {
     // call github api
     $.ajax({type:'GET', dataType:'json', url: 'https://api.github.com/feeds', timeout:5000, success:parseFeed, async: false, beforeSend: function (xhr){ xhr.setRequestHeader('Authorization', make_base_auth(localStorage["username"], localStorage["password"]));}});
@@ -21,6 +30,9 @@ function parseFeed(result) {
     localStorage["feedUrl"] = result.current_user_url;
 }
 
+/**
+ * Get the public feed from Github and parse events
+ */
 function getFeed() {
     $.ajax({type:'GET', dataType:'json', url: localStorage["feedUrl"], timeout:5000, success:parsePrivateFeed, async: false, beforeSend: function (xhr){ xhr.setRequestHeader('Authorization', make_base_auth(localStorage["username"], localStorage["password"]));}});
 }
@@ -37,6 +49,13 @@ function parsePrivateFeed(result) {
     }
 }
 
+/**
+ * Send notification to user.
+ *
+ * @param title The title of the notification
+ * @param text The text of the notification
+ * @param link The link we want to send the user when he clicks the notification
+ */
 function notify(title, text, link) {
     var not = webkitNotifications.createNotification("images/icon-16.png", title, text);
     not.addEventListener("click", function () {
@@ -46,14 +65,20 @@ function notify(title, text, link) {
     not.show();
 }
 
+// initialize the localStorage for lastEntry
 if (localStorage["lastEntry"] == null) {
     localStorage["lastEntry"] = 0;
 }
 
+/**
+ * Method for polling. Gets the public feed and parses events.
+ */
 function run() {
     getFeedUrl();
     getFeed();
     window.setTimeout(run, 60000);
 }
+
+// initial run when extension is ready
 run();
 
