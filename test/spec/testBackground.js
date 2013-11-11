ad35a77fa14d5752420f4b637a3b1d275c9d73c9
Manuel Möhlmann <mail@manmoe.com>
2013-11-11 18:00:00 +0100

    enhanced test

--- test/spec/testBackground.js
+++ test/spec/testBackground.js
@@ -15,29 +15,40 @@
 
             expect(localStorage['feedUrl']).toBe("http://example.com");
         });
-        it('check if parsePublicFeed works', function () {
+        it('check if parsePublicFeed works with createEvents', function () {
             var eventMock = {
                 created_at: "2013-01-01",
                 actor_attributes: {gravatar_id: "ah78agf89af"},
                 type: "CreateEvent",
-                repository: {name: "eventName"},
+                repository: {name: "repoName"},
                 actor: "testActor",
                 url: "http://example.com"
             };
 
+            // add mock to param list
             var eventList = [];
             eventList.push(eventMock);
 
             // mock notify method
             notify = function(title, text, url, gravatarId) {
+                expect(title).toBe("New repository repoName created");
+                expect(text).toBe("testActor has created repoName! Click to get there!");
+                expect(url).toBe("http://example.com");
                 expect(gravatarId).toBe("ah78agf89af");
             }
 
-            isEventActive = function(name) {
+            // all events active
+            isEventActive = function() {
                 return true;
             }
 
+            // mock localStorage
+            localStorage = [];
+            localStorage['lastEntry'] == null;
+
             parsePublicFeed(eventList);
+
+            expect(localStorage['lastEntry']).toBe("2013-01-01");
         });
     });
 })();
