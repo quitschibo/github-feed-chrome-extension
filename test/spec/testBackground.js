44b446f80c839bf31fb21e0b1830a88fcc6d1847
Manuel Möhlmann <mail@manmoe.com>
2013-11-11 15:44:54 +0100

    added new test

--- test/spec/testBackground.js
+++ test/spec/testBackground.js
@@ -1,10 +1,19 @@
-'use strict';
 (function () {
     describe('Test background.js', function () {
         it('check if makeBaseAuth creates a valid basic auth', function () {
             var result = makeBaseAuth("testuser", "testpass");
 
             expect(result).toBe("Basic dGVzdHVzZXI6dGVzdHBhc3M=");
+        });
+        it('check if saveFeedUrl works', function () {
+            localStorage = [];
+            localStorage['feedUrl'] == '';
+
+            feedMock = {current_user_url: "http://example.com"};
+
+            saveFeedUrl(feedMock);
+
+            expect(localStorage['feedUrl']).toBe("http://example.com");
         });
     });
 })();
